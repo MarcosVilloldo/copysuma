1bfdba7529b655e7634100383dae75690ad16749
PC <marcosvilloldo.35122@gmail.com>
2022-05-15 21:59:12 -0300

    Se agrega logica para cuando se hace submit del formulario

--- src/App/components/formulario-de-pedidos/FormularioDePedidos.jsx
+++ src/App/components/formulario-de-pedidos/FormularioDePedidos.jsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import "./FormularioDePedidos.css"
 
 const FormularioDePedidos = () => {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs.precioTotal);
+    }
+
     return (
-        <form class="col-md-12 row">
-            <div class="col-md-3">
+        <form className="col-md-12 row" onSubmit={handleSubmit}>
+            <div className="col-md-2">
                 <label className="col-form-label"> Cliente: 
-                    <input className="form-control" type="text" /> 
+                    <input className="form-control" type="text" name="cliente" value={inputs.cliente || ""} onChange={handleChange}/> 
                 </label>
             </div>
-            <div class="col-md-3">
+            <div className="col-md-2">
                 <label className="col-form-label"> Pedido:
-                    <input className="form-control" type="text" />
+                    <input className="form-control" type="text" name="pedido" value={inputs.pedido || ""} onChange={handleChange}/>
                 </label>
             </div>
-            <div class="col-md-3">
+            <div className="col-md-2">
                 <label className="col-form-label"> Celular:
-                    <input className="form-control" type="text" />
+                    <input className="form-control" type="text" name="celular" value={inputs.celular || ""} onChange={handleChange}/>
                 </label>
             </div>
-            <div class="col-md-2">
+            <div className="col-md-2">
                 <label className="col-form-label"> Precio total:
-                    <input className="form-control" type="number" />
+                    <input className="form-control" type="number" name="precioTotal"  value={inputs.precioTotal || ""}  onChange={handleChange}/>
                 </label>
             </div>
             <button type="submit" className="btn boton-formulario-pedidos"> Confirmar </button>
