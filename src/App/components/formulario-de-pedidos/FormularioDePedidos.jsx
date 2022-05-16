3ccf09c526b9b89e6a3f293c467cd00f449b3a5e
PC <marcosvilloldo.35122@gmail.com>
2022-05-16 00:37:47 -0300

    Se indenta el codigo

--- src/App/components/formulario-de-pedidos/FormularioDePedidos.jsx
+++ src/App/components/formulario-de-pedidos/FormularioDePedidos.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./FormularioDePedidos.css"
 
 const FormularioDePedidos = () => {
@@ -7,7 +7,7 @@ const FormularioDePedidos = () => {
     const handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}))
+        setInputs(values => ({ ...values, [name]: value }))
     }
 
     const handleSubmit = (event) => {
@@ -19,23 +19,23 @@ const FormularioDePedidos = () => {
         <div className="box-formulario">
             <form className="col-md-12 row formulario" onSubmit={handleSubmit}>
                 <div className="col-md-3 dato-formulario">
-                    <label className="col-form-label"> Cliente: 
-                        <input className="form-control input-formulario" type="text" name="cliente" value={inputs.cliente || ""} onChange={handleChange}/> 
+                    <label className="col-form-label"> Cliente:
+                        <input className="form-control input-formulario" type="text" name="cliente" value={inputs.cliente || ""} onChange={handleChange} />
                     </label>
                 </div>
                 <div className="col-md-3 dato-formulario">
                     <label className="col-form-label "> Pedido:
-                        <input className="form-control input-formulario" type="text" name="pedido" value={inputs.pedido || ""} onChange={handleChange}/>
+                        <input className="form-control input-formulario" type="text" name="pedido" value={inputs.pedido || ""} onChange={handleChange} />
                     </label>
                 </div>
                 <div className="col-md-3 dato-formulario">
                     <label className="col-form-label"> Celular:
-                        <input className="form-control input-formulario" type="text" name="celular" value={inputs.celular || ""} onChange={handleChange}/>
+                        <input className="form-control input-formulario" type="text" name="celular" value={inputs.celular || ""} onChange={handleChange} />
                     </label>
                 </div>
                 <div className="col-md-3 dato-formulario">
                     <label className="col-form-label"> Precio total:
-                        <input className="form-control input-formulario" type="number" name="precioTotal"  value={inputs.precioTotal || ""}  onChange={handleChange}/>
+                        <input className="form-control input-formulario" type="number" name="precioTotal" value={inputs.precioTotal || ""} onChange={handleChange} />
                     </label>
                 </div>
                 <div className="col-md-12 box-boton-formulario-pedidos">
