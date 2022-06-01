c901f017fdbd5d5ec8d80478390dfe5cf74763db
PC <marcosvilloldo.35122@gmail.com>
2022-06-01 00:45:25 -0300

    Se modifica la forma en el que se le pasan los datos al modal de modo que se actualicen los datos cuando se hace el sutmit y agrega dependencia de jquery

--- src/App/components/formulario-de-pedidos/FormularioDePedidos.jsx
+++ src/App/components/formulario-de-pedidos/FormularioDePedidos.jsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import "./FormularioDePedidos.css"
 
+
 const FormularioDePedidos = () => {
-    const [inputs, setInputs] = useState({});
-
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
-    }
-
+    const [inputs, setInputs] = useState(
+        {
+            cliente: null,
+            pedido: null,
+            celular: null,
+            importe: 0
+        }
+    );
+    
     const handleSubmit = (event) => {
         event.preventDefault();
+
+        setInputs({...inputs, 
+            cliente: $("#cliente").val(),
+            pedido: $("#pedido").val(),
+            celular: $("#celular").val(),
+            importe:$("#importe").val()
+        });
+
+        $('#modal-pedidos').modal("show");
     }
 
     return (
@@ -19,51 +30,56 @@ const FormularioDePedidos = () => {
             <form className="col-md-12 row formulario" onSubmit={handleSubmit}>
                 <div className="col-md-3 dato-formulario">
                     <label className="col-form-label"> Cliente:
-                        <input className="form-control input-formulario" type="text" name="cliente" value={inputs.cliente || ""} onChange={handleChange} />
+                        <input className="form-control input-formulario" id="cliente" type="text" />
                     </label>
                 </div>
                 <div className="col-md-3 dato-formulario">
                     <label className="col-form-label "> Pedido:
-                        <input className="form-control input-formulario" type="text" name="pedido" value={inputs.pedido || ""} onChange={handleChange} />
+                        <input className="form-control input-formulario" id="pedido" type="text" />
                     </label>
                 </div>
                 <div className="col-md-3 dato-formulario">
                     <label className="col-form-label"> Celular:
-                        <input className="form-control input-formulario" type="text" name="celular" value={inputs.celular || ""} onChange={handleChange} />
+                        <input className="form-control input-formulario" id="celular" type="text" />
                     </label>
                 </div>
                 <div className="col-md-3 dato-formulario">
                     <label className="col-form-label"> Importe:
-                        <input className="form-control input-formulario" type="number" name="importe" value={inputs.importe || ""} onChange={handleChange} />
+                        <input className="form-control input-formulario" id="importe" type="number" />
                     </label>
                 </div>
                 <div className="col-md-12 box-boton-formulario-pedidos">
-                    <button type="submit" className="btn boton-formulario-pedidos" data-toggle="modal" data-target="#myModal"> Ingresar pedido </button>
+                    <button type="submit" className="btn boton-formulario-pedidos"> Ingresar pedido </button>
                 </div>
             </form>
+            {Modal(inputs)}
+        </div>
+    )
+};
 
-            {/* <!-- Modal --> */}
-            <div className="modal fade" id="myModal">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Confirmar pedido</h5>
-                            <button type="button" className="close" data-dismiss="modal">&times;</button>
-                        </div>
-                        <div className="modal-body" role="dialog">
-                            <p><b>Cliente: </b>{inputs.cliente}</p>
-                            <p><b>Pedido: </b>{inputs.pedido}</p>
-                            <p><b>Celular: </b>{inputs.celular}</p>
-                            <p><b>Importe: </b>{inputs.importe}</p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                        </div>
+const Modal = (inputs) => {
+    return (
+        <div className="modal fade" id="modal-pedidos">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Confirmar pedido</h5>
+                        <button type="button" className="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div className="modal-body" id="datos-modal" role="dialog">
+                        <p><b>Cliente: </b>{inputs.cliente}</p>
+                        <p><b>Pedido:  </b>{inputs.pedido} </p>
+                        <p><b>Celular: </b>{inputs.celular}</p>
+                        <p><b>Importe: </b>{inputs.importe}</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
                     </div>
                 </div>
             </div>
         </div>
     )
 };
+
 
 export default FormularioDePedidos;
