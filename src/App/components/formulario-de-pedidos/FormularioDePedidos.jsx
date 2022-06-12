7142b0f0963c93945d13c6e22e6364630a7bb0a5
PC <marcosvilloldo.35122@gmail.com>
2022-06-12 03:47:55 -0300

    Se agrega restriccion para ingresar el formulario de pedidos

--- src/App/components/formulario-de-pedidos/FormularioDePedidos.jsx
+++ src/App/components/formulario-de-pedidos/FormularioDePedidos.jsx
@@ -11,13 +11,14 @@ const inputsDefault = {
 }
 
 const FormularioDePedidos = (props) => {
-    const { register, errors, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
     const [inputs, setInputs] = useState(inputsDefault);
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const handleError = (errors) => {};
 
     const onSubmit = (data) => {
         setInputs({
@@ -34,24 +35,28 @@ const FormularioDePedidos = (props) => {
     return (
         <>
             <div className="box-formulario">
-                <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form onSubmit={handleSubmit(onSubmit, handleError)}>
                     <Row className="row-datos-formulario" md="12" as={Col}>
                         <Form.Group className="dato-formulario" md="6" as={Col} >
                             <Form.Label>Cliente</Form.Label>
-                            <Form.Control className="input-formulario-cliente" type="text" placeholder="Ingresar cliente..." {...register('cliente')} />
+                            <Form.Control className="input-formulario-cliente" type="text" placeholder="Ingresar cliente..." name="cliente" {...register('cliente', {required: "Cliente es requerido" })} />
+                            <div className="span-formulario"><span className="span-formulario text-danger"> {errors?.cliente && errors.cliente.message} </span></div>
                         </Form.Group>
                         <Form.Group className="dato-formulario" md="3" as={Col} >
                             <Form.Label>Celular</Form.Label>
-                            <Form.Control className="input-formulario-celular" type="text" placeholder="Ingresar celular..." {...register('celular')} />
+                            <Form.Control className="input-formulario-celular" type="text" placeholder="Ingresar celular..." name="celular" {...register('celular', {required: "Celular es requerido" })} />
+                            <div className="span-formulario"><span className="span-formulario text-danger"> {errors?.celular && errors.celular.message} </span></div>
                         </Form.Group>
                         <Form.Group className="dato-formulario" md="3" as={Col} >
                             <Form.Label>Importe</Form.Label>
-                            <Form.Control className="input-formulario-importe" type="number" placeholder="Ingresar importe..." {...register('importe')} />
+                            <Form.Control className="input-formulario-importe" type="number" placeholder="Ingresar importe..." name="importe" {...register('importe', {required: "Importe es requerido" })} />
+                            <div className="span-formulario"> <span className="text-danger"> {errors?.importe && errors.importe.message} </span></div>
                         </Form.Group>
                     </Row>
                     <Form.Group className="dato-formulario">
                         <Form.Label>Pedido</Form.Label>
-                        <Form.Control className="input-formulario-pedido" type="text" placeholder="Ingresar pedido..." {...register('pedido')} />
+                        <Form.Control className="input-formulario-pedido" type="text" placeholder="Ingresar pedido..." name="pedido" {...register('pedido', {required: "Pedido es requerido" })} />
+                        <div className="span-formulario"><span className="span-formulario text-danger"> {errors?.pedido && errors.pedido.message} </span></div>
                     </Form.Group>
                     <Form.Group className="box-boton-formulario-pedidos">
                         <Button className="boton-formulario-pedidos" variant="dark" type="submit"> Ingresar pedido </Button>
