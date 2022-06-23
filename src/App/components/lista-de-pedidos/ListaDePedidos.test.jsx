27010e3ef054353137a9cf4b0abce4e9f8bf16bc
PC <marcosvilloldo.35122@gmail.com>
2022-06-23 00:17:51 -0300

    Se agrega helper para lista de pedidos test y agrega test para probar que se crea bien la lista de pedidos con los valores ingresados

--- src/App/components/lista-de-pedidos/ListaDePedidos.test.jsx
+++ src/App/components/lista-de-pedidos/ListaDePedidos.test.jsx
@@ -1,25 +1,49 @@
 import { render, screen } from '@testing-library/react'
 import ListaDePedidos from './ListaDePedidos'
-import jsonPedidos from "../../helpers/pedidos.json"
+import jsonPedidos from "../../helpers/pedidos-test.json"
 
 const boton = {
     botonAnterior: "hidden",
     botonSiguiente: "hidden"
 };
 
-const paginaSiguiente = () => {}
-const paginaAnterior = () => {}
+const paginaSiguiente = () => { }
+const paginaAnterior = () => { }
 
 describe('Test para probar lista de pedidos', () => {
-    it('Deberia renderizar el encabezado de la lista de pedidos', () => {
-        render(<ListaDePedidos paginaActiva={1} pedidos={jsonPedidos.pedidos} paginaSiguiente={paginaSiguiente} paginaAnterior={paginaAnterior} boton={boton}/>)
-      
+
+    it('Deberia renderizar el componente ListaDePedidos', () => {
+        render(<ListaDePedidos paginaActiva={1} pedidos={jsonPedidos.pedidos} paginaSiguiente={paginaSiguiente} paginaAnterior={paginaAnterior} boton={boton} />)
+
         expect(screen.getByText("Lista de pedidos"))
     })
 
-    it('Deberia renderizar una lista de 10 pedidos', () => {
-        render(<ListaDePedidos paginaActiva={1} pedidos={jsonPedidos.pedidos} paginaSiguiente={paginaSiguiente} paginaAnterior={paginaAnterior} boton={boton}/>)
-    
-        expect(screen.getAllByTestId("item-pedido")).toHaveLength(10);
+    it('Deberia renderizar una lista de 2 pedidos', () => {
+        const contenedor = render(<ListaDePedidos paginaActiva={1} pedidos={jsonPedidos.pedidos} paginaSiguiente={paginaSiguiente} paginaAnterior={paginaAnterior} boton={boton} />)
+
+        expect(contenedor.container.querySelectorAll('#item-pedido')).toHaveLength(2);
     })
+
+    it('Deberia obtener el primer valor de la lista de pedidos y ser igual al esperado', () => {
+        const contenedor = render(<ListaDePedidos paginaActiva={1} pedidos={jsonPedidos.pedidos} paginaSiguiente={paginaSiguiente} paginaAnterior={paginaAnterior} boton={boton} />)
+
+        const pedidoEsperado = {
+            cliente: ' Marcos ',
+            celular: ' 11123232 ',
+            pedido: ' El señor de los anillos ',
+            importe: ' $ 200 '
+        }
+
+        const pedidosObtenidos = contenedor.container.querySelectorAll('#item-pedido');
+
+        const pedidoObtenido = {
+            cliente: pedidosObtenidos.item(0).querySelector('#cliente').textContent,
+            celular: pedidosObtenidos.item(0).querySelector('#celular').textContent,
+            pedido: pedidosObtenidos.item(0).querySelector('#pedido').textContent,
+            importe: pedidosObtenidos.item(0).querySelector('#importe').textContent
+        }
+
+        expect(pedidoObtenido).toEqual(pedidoEsperado);
+    })
+
 })
