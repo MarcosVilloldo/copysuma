ff9363a9ee5c8a34f1fb9aed1f3552ae9cf8a806
PC <marcosvilloldo.35122@gmail.com>
2022-06-27 20:31:16 -0300

    Se agrega logica de lista de pedidos preparados

--- src/App/components/lista-de-pedidos-preparados/ListaDePreparados.jsx
+++ src/App/components/lista-de-pedidos-preparados/ListaDePreparados.jsx
@@ -1,16 +1,63 @@
 import React from "react";
-import { ListGroup } from 'react-bootstrap';
+import { ListGroup, Button, Row, Col } from 'react-bootstrap';
 import './ListaDePreparados.css'
 
-const ListaDePreparados = () => {
+var paginas;
+
+const ListaDePreparados = (props) => {
     return (
         <>
             <ListGroup className="lista-de-pedidos-preparados">
                 <ListGroup.Item className="titulo-lista-pedidos-preparados"> Lista de pedidos preparados </ListGroup.Item>
+                {agregarItemsAListaDePedidos(obtenerListaDePedidos(props.pedidos), props.paginaActiva, props.finalizarPedido)}
+                <ListGroup.Item className="pedido">
+                    <Row>
+                        <Col className="box-boton-anterior">
+                            <Button className="boton-lista-pedidos" variant="dark" onClick={props.paginaAnterior} style={{ visibility: props.boton.botonAnterior }}>
+                                <i className="bi bi-arrow-left"></i>
+                            </Button>
+                        </Col>
+                        <Col className="box-numero-pagina" id="paginado"> {props.paginaActiva} / {paginas} </Col>
+                        <Col className="box-boton-siguiente">
+                            <Button className="boton-lista-pedidos" variant="dark" onClick={() => props.paginaSiguiente(paginas)} style={{ visibility: props.boton.botonSiguiente }}>
+                                <i className="bi bi-arrow-right"></i>
+                            </Button>
+                        </Col>
+                    </Row>
+                </ListGroup.Item>
             </ListGroup>
         </>
     );
 };
 
+const agregarItemsAListaDePedidos = (pedidos, paginaActiva, finalizarPedido) => {
+    return pedidos.get(paginaActiva).map((pedido, cantidad) => (
+        <ListGroup id="item-pedido" key={cantidad.toString()} horizontal>
+            <ListGroup.Item className="rounded-0 boton-item-pedido-preparado" id="cliente" md="2" as={Col}> {pedido.cliente} </ListGroup.Item>
+            <ListGroup.Item className="rounded-0 boton-item-pedido-preparado" id="celular" md="2" as={Col}> {pedido.celular} </ListGroup.Item>
+            <ListGroup.Item className="rounded-0 boton-item-pedido-preparado" id="pedido" md="6" as={Col}> {pedido.pedido} </ListGroup.Item>
+            <ListGroup.Item className="rounded-0 boton-item-pedido-preparado" id="importe" md="1" as={Col}> $ {pedido.importe} </ListGroup.Item>
+            <ListGroup.Item className="rounded-0 item-finalizar-pedido" md="1" as={Col}>
+                {pedido.finalizado === false
+                    ? <Button className="boton-finalizar-pedido" variant="dark" onClick={() => finalizarPedido(pedido.id)}>finalizar</Button>
+                    : <Button className="boton-finalizar-pedido" variant="dark" onClick={() => finalizarPedido(pedido.id)} disabled>finalizado</Button>
+                }
+            </ListGroup.Item>
+        </ListGroup>
+    ))
+}
+
+const obtenerListaDePedidos = (pedidos) => {
+    paginas = Math.ceil(pedidos.length / 10);
+
+    let paginado = new Map();
+    let iteraciones = 0;
+    while (iteraciones < paginas) {
+        paginado.set((iteraciones + 1), pedidos.slice(iteraciones + '0', (iteraciones + 1) + '0'));
+        iteraciones++;
+    }
+
+    return paginado;
+}
 
 export default ListaDePreparados;
--- src/App/layout/preparados/preparados.jsx
+++ src/App/layout/preparados/preparados.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import ListaDePreparados from "../../components/lista-de-pedidos-preparados/ListaDePreparados";
+import jsonPedidos from "../../helpers/pedidos.json"
 
 const Preparados = () => {
+    const [pedidos, setPedidos] = useState(jsonPedidos.pedidos);
+
+    const estadoBotonSiguiente = pedidos.length > 10 ? "visible" : "hidden";
+
+    const [boton, setBoton] = useState({
+        botonAnterior: "hidden",
+        botonSiguiente: estadoBotonSiguiente
+    });
+
+    const [paginaActiva, setPaginaActiva] = useState(1);
+
+    const finalizarPedido = (idPedido) => setPedidos(pedidos.map((pedido) => pedido.id === idPedido ? { ...pedido, finalizado: true } : { ...pedido }));
+
+    const paginaSiguiente = (paginas) => {
+        let paginaActivaNueva = paginaActiva + 1;
+        setPaginaActiva(paginaActivaNueva);
+        paginas <= paginaActivaNueva ? setBoton({ botonSiguiente: "hidden" }) : setBoton({ botonSiguiente: "visible" });
+
+        return paginaActiva;
+    }
+
+    const paginaAnterior = () => {
+        let paginaActivaNueva = paginaActiva - 1;
+        setPaginaActiva(paginaActivaNueva);
+        paginaActivaNueva == 1 ? setBoton({ botonAnterior: "hidden" }) : setBoton({ botonAnterior: "visible" });
+
+        return paginaActiva;
+    }
+
     return (
         <>
-            <ListaDePreparados />
+            <ListaDePreparados pedidos={pedidos} paginaActiva={paginaActiva} paginaSiguiente={paginaSiguiente} paginaAnterior={paginaAnterior} boton={boton} finalizarPedido={finalizarPedido} />
         </>
     );
 };
