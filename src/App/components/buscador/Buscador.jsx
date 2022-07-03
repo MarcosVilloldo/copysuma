9677bdef462307b9b30da6b90c1e0cef3c4f519f
PC <marcosvilloldo.35122@gmail.com>
2022-07-03 01:45:06 -0300

    Se modifica el boton de buscar para que tenga la opcion de selecconar el tipo de busqueda

--- src/App/components/buscador/Buscador.jsx
+++ src/App/components/buscador/Buscador.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Form, Row, Col } from 'react-bootstrap';
+import { Button, ButtonGroup, Dropdown, Form, Row, Col } from 'react-bootstrap';
 import "./Buscador.css"
 
 const Buscador = () => {
@@ -7,12 +7,20 @@ const Buscador = () => {
         <>
             <Form className="box-buscador-pedidos-preparados">
                 <Row className="row-datos-buscador" md="12">
-                    <Form.Group md="11" as={Col} >
+                    <Col md="10">
                         <Form.Control className="input-buscador" type="text" placeholder="Ingresar busqueda..." />
-                    </Form.Group>
-                    <Form.Group className="box-boton-buscar" md="1" as={Col} >
-                        <Button className="boton-buscar" variant="dark" type="submit"> Buscar <i className="bi bi-search"></i></Button>
-                    </Form.Group>
+                    </Col>
+                    <Col md="2">
+                        <Dropdown className="box-boton-buscar" md="3" as={ButtonGroup}>
+                            <Button className="boton-buscar" variant="dark" type="submit">Buscar por pedido<i className="bi bi-search"></i></Button>
+                            <Dropdown.Toggle className="dropdown-buscar" variant="dark" split />
+                            <Dropdown.Menu className="dropdown-buscar-menu-items" variant="dark">
+                                <Dropdown.Item >Pedido</Dropdown.Item>
+                                <Dropdown.Item >Cliente</Dropdown.Item>
+                                <Dropdown.Item >Celular</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
                 </Row>
             </Form>
         </>
