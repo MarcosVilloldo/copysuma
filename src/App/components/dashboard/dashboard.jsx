66f3d2a5b3d3ad129070fd0fb61add7c020abf76
PC <marcosvilloldo.35122@gmail.com>
2022-07-07 23:29:05 -0300

    Se realiza primera instrancia de dashboard

--- src/App/components/dashboard/dashboard.jsx
+++ src/App/components/dashboard/dashboard.jsx
@@ -1,26 +1,60 @@
 import React from "react";
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from "chart.js";
-import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from "chart.js";
+import { Line, Pie } from 'react-chartjs-2';
+import { Row, Col } from 'react-bootstrap';
+import jsonPedidos from "../../helpers/pedidos.json"
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler)
+ChartJS.register(ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler)
 
-const Dashboard = () => (
-    <>
-        <Line
-            datasetIdKey= {1}
-            data={{
-                labels: ['Jun', 'Jul', 'Aug'],
-                datasets: [
-                    {
-                        id: 1,
-                        label: '',
-                        data: [5, 6, 7],
-                        borderColor: "rgb(75,192,192)"
-                    },
-                ],
-            }}
-        />
-    </>
-);
+const Dashboard = () => {
+    return (
+        <Row>
+            <Col md="6">
+                <Row md="6">
+                    <h2>Recaudado: <b>9599</b></h2>
+                  
+                </Row>
+                <Row md="6">
+                    <Line
+                        datasetIdKey={1}
+                        data={{
+                            labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dic'],
+                            datasets: [
+                                {
+                                    id: 'id',
+                                    label: 'Pedidos',
+                                    tension: 0.3,
+                                    data: [5, 6, 7],
+                                    pointRadius: 6,
+                                    borderColor: "rgb(75,192,192)",
+                                    backgroundColor: "rgb(205,203,202)"
+                                },
+                            ],
+                        }}
+                    />
+                </Row>
+            </Col>
+            <Col md="6">
+                <Pie
+                    data={{
+                        labels: ['Libros', 'Fotocopias', 'Otros'],
+                        datasets: [
+                            {
+                                id: 1,
+                                label: '',
+                                data: [55, 16, 7],
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                    'rgba(54, 162, 235, 0.2)',
+                                    'rgba(255, 206, 86, 0.2)'
+                                ],
+                            },
+                        ],
+                    }}
+                />
+            </Col>
+        </Row>
+    );
+};
 
 export default Dashboard;
