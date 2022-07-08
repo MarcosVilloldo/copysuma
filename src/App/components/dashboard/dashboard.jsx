e34331a8a7bc735eb88006b532078092aa3729af
PC <marcosvilloldo.35122@gmail.com>
2022-07-08 00:52:47 -0300

    Se agrega logica para que en el grafico de lineas se muestre la cantidad de pedidos que se hicieron por dia

--- src/App/components/dashboard/dashboard.jsx
+++ src/App/components/dashboard/dashboard.jsx
@@ -14,19 +14,19 @@ const Dashboard = () => {
         <Row>
             <Col md="6">
                 <Row className="box-cantidad-recaudacion" md="6">
-                    <h2>Recaudado <b>9599</b></h2>
+                    <h2>Recaudado <b> {calcularTotalRecaudado()} </b></h2>
                 </Row>
                 <Row md="6">
                     <Line
                         datasetIdKey={1}
                         data={{
-                            labels: jsonPedidos.pedidos.map((pedido) => formatearFecha(pedido.fecha)),
+                            labels: jsonPedidos.pedidos.map((pedido) => formatearFecha(pedido.fecha)).reverse(),
                             datasets: [
                                 {
                                     id: 'id',
                                     label: 'Pedidos',
                                     tension: 0.3,
-                                    data: jsonPedidos.pedidos.map((pedido) => pedido.importe ),
+                                    data: pedidosDiarios(),
                                     pointRadius: 6,
                                     borderColor: "rgb(75,192,192)",
                                     backgroundColor: "rgb(205,203,202)"
@@ -58,5 +58,27 @@ const Dashboard = () => {
         </Row>
     );
 };
+
+const calcularTotalRecaudado = () => {
+    let totalRecaudado = 0;
+    jsonPedidos.pedidos.map((pedido) => {
+        totalRecaudado = totalRecaudado + pedido.importe;
+    })
+
+    return totalRecaudado;
+}
+
+const pedidosDiarios = () => {
+    let pedidosPorDia = [];
+    let fechaAux = null;
+
+    jsonPedidos.pedidos.map((pedido) => {
+        if(pedido.fecha !== fechaAux){
+            fechaAux = pedido.fecha;
+            pedidosPorDia.push(jsonPedidos.pedidos.filter(i => i.fecha === pedido.fecha).length);
+        }
+    })
+    return pedidosPorDia;
+}
 
 export default Dashboard;
