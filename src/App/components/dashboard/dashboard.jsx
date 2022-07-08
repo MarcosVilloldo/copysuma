f44aed1e17eaadefdd86823df92854abf3e4497a
PC <marcosvilloldo.35122@gmail.com>
2022-07-08 02:23:32 -0300

    Modifica estilos del dashboard

--- src/App/components/dashboard/dashboard.jsx
+++ src/App/components/dashboard/dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from "chart.js";
 import { Line, Pie } from 'react-chartjs-2';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Card } from 'react-bootstrap';
 import { formatearFecha } from '../../utils/formateador-de-fecha.js';
 import jsonPedidos from "../../helpers/pedidos.json"
 
@@ -11,32 +11,62 @@ ChartJS.register(ArcElement, CategoryScale, LinearScale, PointElement, LineEleme
 
 const Dashboard = () => {
     return (
-        <Row>
+        <Row className="box-dashboard">
             <Col md="6">
-                <Row className="box-cantidad-recaudacion" md="6">
-                    <h2>Recaudado <b> {calcularTotalRecaudado()} </b></h2>
+                <Row className="box-cantidad-recaudacion" md="6" >
+                    <Col className="card-recaudacion" md="6">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title> Recaudación diaria </Card.Title>
+                                <Card.Text style={{ fontSize: '32px' }}><b>{'$ ' + calcularTotalRecaudado()}</b></Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className="card-recaudacion" md="6">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title> Recaudación total </Card.Title>
+                                <Card.Text style={{ fontSize: '32px' }}><b>{'$ ' + 0}</b></Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
                 </Row>
-                <Row md="6">
+                <Row className="box-grafico-de-lineas" md="6">
                     <Line
                         datasetIdKey={1}
                         data={{
-                            labels: jsonPedidos.pedidos.map((pedido) => formatearFecha(pedido.fecha)).reverse(),
+                            labels: jsonPedidos.pedidos.map((pedido) => formatearFecha(pedido.fecha)),
                             datasets: [
                                 {
                                     id: 'id',
                                     label: 'Pedidos',
                                     tension: 0.3,
                                     data: pedidosDiarios(),
-                                    pointRadius: 6,
+                                    pointRadius: 4,
                                     borderColor: "rgb(75,192,192)",
                                     backgroundColor: "rgb(205,203,202)"
                                 },
                             ],
                         }}
+                        options={{
+                            fill: true,
+                            scales: {
+                                y: {
+                                    min: 0
+                                }
+                            },
+                            plugins: {
+                                legend: {
+                                    display:false
+                                }
+                            },
+                            width:"40"
+
+                        }}
                     />
                 </Row>
             </Col>
-            <Col md="6">
+            <Col className="columna-pie" md="6">
                 <Pie
                     data={{
                         labels: ['Libros', 'Fotocopias', 'Otros'],
@@ -73,11 +103,12 @@ const pedidosDiarios = () => {
     let fechaAux = null;
 
     jsonPedidos.pedidos.map((pedido) => {
-        if(pedido.fecha !== fechaAux){
+        if (pedido.fecha !== fechaAux) {
             fechaAux = pedido.fecha;
             pedidosPorDia.push(jsonPedidos.pedidos.filter(i => i.fecha === pedido.fecha).length);
         }
     })
+
     return pedidosPorDia;
 }
 
