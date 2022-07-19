4d91a38e2b90d922b0ce3d45cdf9d1458c431455
PC <marcosvilloldo.35122@gmail.com>
2022-07-18 21:45:44 -0300

    Se crea primera instancia de componente accordion para la vista de home

--- src/App/layout/home/Home.jsx
+++ src/App/layout/home/Home.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import { Accordion } from 'react-bootstrap';
 import ListaDePedidos from "../../components/lista-de-pedidos/ListaDePedidos";
 import FormularioDePedidos from "../../components/formulario-de-pedidos/FormularioDePedidos";
+import Buscador from "../../components/buscador/Buscador";
 import jsonPedidos from "../../helpers/pedidos.json";
+
+const filtro = { PEDIDO: 'pedido', CLIENTE: 'cliente', CELULAR: 'celular' }
 
 const Home = () => {
     const [pedidos, setPedidos] = useState(jsonPedidos.pedidos);
+    const [filtroDeBusqueda, setFiltroDeBusqueda] = useState(filtro.PEDIDO);
     const [boton, setBoton] = useState({ botonAnterior: "hidden", botonSiguiente: pedidos.length > 10 ? "visible" : "hidden" });
     const [paginaActiva, setPaginaActiva] = useState(1);
 
@@ -18,6 +23,10 @@ const Home = () => {
         if (pedidos.length >= (paginaActiva * 10)) { setBoton({ botonSiguiente: "visible" }) }
         if (paginaActiva == 1) { setBoton({ botonAnterior: "hidden" }) }
     };
+
+    const filtrarPedidos = (busqueda) => { };
+
+    const modificarFiltroBusqueda = (filtro) => { };
 
     const cambiarPagina = (orientacion, paginas) => {
         let paginaActivaNueva;
@@ -39,10 +48,25 @@ const Home = () => {
 
     return (
         <>
-            <FormularioDePedidos agregarPedido={agregarPedido} />
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header> Buscador </Accordion.Header>
+                    <Accordion.Body>
+                        <Buscador filtros={[filtro.PEDIDO, filtro.CLIENTE, filtro.CELULAR]}
+                            filtroDeBusqueda={filtroDeBusqueda}
+                            filtrar={filtrarPedidos}
+                            modificarFiltroBusqueda={modificarFiltroBusqueda} />
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header> Formulario para ingresar pedido </Accordion.Header>
+                    <Accordion.Body>
+                        <FormularioDePedidos agregarPedido={agregarPedido} />
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
             <hr />
-            <ListaDePedidos
-                boton={boton}
+            <ListaDePedidos boton={boton}
                 pedidos={pedidos}
                 paginaActiva={paginaActiva}
                 cambiarPagina={cambiarPagina}
