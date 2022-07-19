7dc7e77f5ce61a58ce4dde0a8e6bded28b2250bd
PC <marcosvilloldo.35122@gmail.com>
2022-07-19 00:14:55 -0300

    Se agrega logica de buscador para la lista de pedidos

--- src/App/layout/home/Home.jsx
+++ src/App/layout/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Accordion } from 'react-bootstrap';
 import ListaDePedidos from "../../components/lista-de-pedidos/ListaDePedidos";
 import FormularioDePedidos from "../../components/formulario-de-pedidos/FormularioDePedidos";
@@ -11,8 +11,27 @@ const filtro = { PEDIDO: 'pedido', CLIENTE: 'cliente', CELULAR: 'celular' }
 const Home = () => {
     const [pedidos, setPedidos] = useState(jsonPedidos.pedidos);
     const [filtroDeBusqueda, setFiltroDeBusqueda] = useState(filtro.PEDIDO);
-    const [boton, setBoton] = useState({ botonAnterior: "hidden", botonSiguiente: pedidos.length > 10 ? "visible" : "hidden" });
+    const [textoBusqueda, setTextBusqueda] = useState('');
     const [paginaActiva, setPaginaActiva] = useState(1);
+    const [boton, setBoton] = useState({ botonAnterior: "hidden", botonSiguiente: pedidos.length > 10 ? "visible" : "hidden" });
+
+    useEffect(() => {
+        let pedidosFiltrados;
+
+        if (filtroDeBusqueda === filtro.PEDIDO) pedidosFiltrados = pedidos.filter(pedido => pedido.pedido.toLocaleLowerCase() === textoBusqueda.toLocaleLowerCase());
+        if (filtroDeBusqueda === filtro.CLIENTE) pedidosFiltrados = pedidos.filter(pedido => pedido.cliente.toLocaleLowerCase() === textoBusqueda.toLocaleLowerCase());
+        if (filtroDeBusqueda === filtro.CELULAR) pedidosFiltrados = pedidos.filter(pedido => pedido.celular.toLocaleLowerCase() === textoBusqueda.toLocaleLowerCase());
+
+        if (pedidosFiltrados.length > 0) {
+            setPedidos(pedidosFiltrados);
+            setPaginaActiva(1);
+            setBoton({ botonAnterior: 'hidden', botonSiguiente: pedidosFiltrados.length > 10 ? 'visible' : 'hidden' });
+        } else {
+            setPedidos(jsonPedidos.pedidos);
+            setBoton({ botonAnterior: paginaActiva === 1 ? 'hidden' : 'visible', botonSiguiente: jsonPedidos.pedidos.length > 10 * paginaActiva ? 'visible' : 'hidden' });
+        }
+
+    }, [textoBusqueda, filtroDeBusqueda]);
 
     const agregarPedido = (pedidoNuevo) => {
         let fechaActual = new Date();
@@ -25,9 +44,9 @@ const Home = () => {
         if (paginaActiva == 1) { setBoton({ botonAnterior: "hidden" }) }
     };
 
-    const filtrarPedidos = (busqueda) => { };
+    const filtrarPedidos = (busqueda) => setTextBusqueda(busqueda);
 
-    const modificarFiltroBusqueda = (filtro) => { };
+    const modificarFiltroBusqueda = (filtro) => setFiltroDeBusqueda(filtro);
 
     const cambiarPagina = (orientacion, paginas) => {
         let paginaActivaNueva;
@@ -49,20 +68,20 @@ const Home = () => {
 
     return (
         <>
-            <Accordion defaultActiveKey="0">
+            <Accordion defaultActiveKey="1">
                 <Accordion.Item className="accordion-item-home" eventKey="0">
+                    <Accordion.Header className="accordion-header-home"> Formulario para ingresar pedido </Accordion.Header>
+                    <Accordion.Body className="accordion-body-home">
+                        <FormularioDePedidos agregarPedido={agregarPedido} />
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item className="accordion-item-home" eventKey="1">
                     <Accordion.Header className="accordion-header-home"> Buscador </Accordion.Header>
                     <Accordion.Body className="accordion-body-home">
                         <Buscador filtros={[filtro.PEDIDO, filtro.CLIENTE, filtro.CELULAR]}
                             filtroDeBusqueda={filtroDeBusqueda}
                             filtrar={filtrarPedidos}
                             modificarFiltroBusqueda={modificarFiltroBusqueda} />
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item className="accordion-item-home" eventKey="1">
-                    <Accordion.Header className="accordion-header-home"> Formulario para ingresar pedido </Accordion.Header>
-                    <Accordion.Body className="accordion-body-home">
-                        <FormularioDePedidos agregarPedido={agregarPedido} />
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
