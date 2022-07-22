041033cd363f0e2a20e4f5f6c60613d36e855a9b
PC <marcosvilloldo.35122@gmail.com>
2022-07-21 23:57:15 -0300

    Se agrega contexto de donde se guardan las portadas para mostrar como imagen

--- src/App/components/lista-de-libros/ListaDeLibros.jsx
+++ src/App/components/lista-de-libros/ListaDeLibros.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Card, Row, Col } from 'react-bootstrap';
 import "./ListaDeLibros.css"
 
+const RUTA_PORTADAS = require.context('../../img', true);
+
 const ListaDeLibros = (props) => {
     return (
         <Row xs={1} md={4} className="g-4">
             {props.biblioteca.map((producto, indice) => (
                 <Col key={indice}>
                     <Card >
-                        <Card.Img variant="top" src={producto.portada} />
+                        <Card.Img variant="top" src={RUTA_PORTADAS(producto.portada)} />
                         <Card.Body>
                             <Card.Title>{producto.titulo}</Card.Title>
                             <Card.Text>{producto.descripcion}</Card.Text>
