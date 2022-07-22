f0a1c74146ce8baa06f002458ed94ef9441e3ba8
PC <marcosvilloldo.35122@gmail.com>
2022-07-22 01:11:01 -0300

    Se agrega estilos a las cards

--- src/App/components/lista-de-libros/ListaDeLibros.jsx
+++ src/App/components/lista-de-libros/ListaDeLibros.jsx
@@ -6,10 +6,10 @@ const RUTA_PORTADAS = require.context('../../img', true);
 
 const ListaDeLibros = (props) => {
     return (
-        <Row xs={1} md={4} className="g-4">
+        <Row xs={1} md={4} className="lista-modulos">
             {props.biblioteca.map((modulo, indice) => (
                 <Col key={indice}>
-                    <Card >
+                    <Card className="modulo">
                         <Card.Img variant="top" src={RUTA_PORTADAS(modulo.portada)} />
                         <Card.Body>
                             <Card.Title>{modulo.titulo}</Card.Title>
