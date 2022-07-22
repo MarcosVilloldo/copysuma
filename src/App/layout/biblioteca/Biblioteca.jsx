46d9a20792f9d7c45d1a86e217af728bcd36b9ba
PC <marcosvilloldo.35122@gmail.com>
2022-07-21 22:31:31 -0300

    Se agrega buscador a la biblioteca

--- src/App/layout/biblioteca/Biblioteca.jsx
+++ src/App/layout/biblioteca/Biblioteca.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Accordion } from 'react-bootstrap';
 import ListaDeLibros from "../../components/lista-de-libros/ListaDeLibros";
+import Buscador from "../../components/buscador/Buscador";
 
-const Biblioteca = () => (
-    <>
-        <ListaDeLibros />
-    </>
-);
+const Biblioteca = () => {
+    const [filtroDeBusqueda, setFiltroDeBusqueda] = useState(['titulo']);
+    const [textoBusqueda, setTextBusqueda] = useState('');
+
+    const filtrarPedidos = (busqueda) => setTextBusqueda(busqueda);
+
+    const modificarFiltroBusqueda = (filtro) => setFiltroDeBusqueda(filtro);
+
+    return (
+        <>
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item className="accordion-item-preparados" eventKey="0">
+                    <Accordion.Header className="accordion-header-preparados"> Buscador </Accordion.Header>
+                    <Accordion.Body className="accordion-body-preparados">
+                        <Buscador filtros={['titulo']} filtroDeBusqueda={filtroDeBusqueda} filtrar={filtrarPedidos} modificarFiltroBusqueda={modificarFiltroBusqueda} />
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+            <hr />
+            <ListaDeLibros />
+        </>
+    );
+};
 
 export default Biblioteca;
