8d04809c360c1fe200534977727d50e9ee2ec554
PC <marcosvilloldo.35122@gmail.com>
2022-07-27 00:58:26 -0300

    Sse hace refactor del formateador de fechas

--- src/App/utils/formateador-de-fecha.js
+++ src/App/utils/formateador-de-fecha.js
@@ -1,20 +1,17 @@
 export const formatearFecha = (fecha) => {
     let date = new Date(fecha);
 
-    return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+    return date.toLocaleDateString();
 }
 
 export const formatearFechaToISOString = (fecha) => {
-    let date = new Date(fecha);
-    let dia = date.getDate() + 1;
-    date.setDate(dia)
+    let date = new Date(fecha + 'T00:00:00');
 
     return date.toISOString();
 }
 
 export const obtenerFormatoParaMostrar = (fecha) => {
     let date = new Date(fecha);
-    let fechaLimpia = date.toISOString().split('T')[0];
 
-    return  fechaLimpia;
+    return date.toISOString().split('T')[0];
 }
