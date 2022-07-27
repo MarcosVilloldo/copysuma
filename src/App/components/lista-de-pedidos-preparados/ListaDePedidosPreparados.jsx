c7c136b5f20f494b438706d397ba73d898484e40
PC <marcosvilloldo.35122@gmail.com>
2022-07-27 02:40:13 -0300

    Se agrega descripcion de cada columna para la lista de pedidos preparados

--- src/App/components/lista-de-pedidos-preparados/ListaDePedidosPreparados.jsx
+++ src/App/components/lista-de-pedidos-preparados/ListaDePedidosPreparados.jsx
@@ -8,22 +8,33 @@ const ListaDePedidosPreparados = (props) => {
     const paginado = obtenerPaginado(props.pedidos, props.paginas);
 
     return (
-        paginado.size > 0 ?
-            paginado.get(props.paginaActiva).map((pedido, indice) => (
-                <ListGroup className="item-fila" key={indice.toString()} horizontal>
-                    <ListGroup.Item className="item-columna" md="2" as={Col}> {formatearFecha(pedido.fechaEntrega)} </ListGroup.Item>
-                    <ListGroup.Item className="item-columna" md="2" as={Col}> {pedido.cliente} </ListGroup.Item>
-                    <ListGroup.Item className="item-columna" md="2" as={Col}> {pedido.celular} </ListGroup.Item>
-                    <ListGroup.Item className="item-columna" md="4" as={Col}> {pedido.pedido} </ListGroup.Item>
-                    <ListGroup.Item className="item-columna" md="1" as={Col}> $ {pedido.importe} </ListGroup.Item>
-                    <ListGroup.Item className="item-boton-finalizar" md="1" as={Col}>
-                        {pedido.finalizado === false
-                            ? <Button className="boton-finalizar-pedido" variant="dark" onClick={() => props.finalizarPedido(pedido.id)}>finalizar</Button>
-                            : <Button className="boton-finalizar-pedido" variant="dark" onClick={() => props.finalizarPedido(pedido.id)} disabled>finalizado</Button>
-                        }
-                    </ListGroup.Item>
-                </ListGroup>
-            )) : null
+        <>
+            <ListGroup className="item-fila" horizontal>
+                <ListGroup.Item className="item-columna-header" md="2" as={Col}> Entrega </ListGroup.Item>
+                <ListGroup.Item className="item-columna-header" md="2" as={Col}> Cliente </ListGroup.Item>
+                <ListGroup.Item className="item-columna-header" md="2" as={Col}> Celular </ListGroup.Item>
+                <ListGroup.Item className="item-columna-header" md="4" as={Col}> Pedido </ListGroup.Item>
+                <ListGroup.Item className="item-columna-header" md="1" as={Col}> Importe </ListGroup.Item>
+                <ListGroup.Item className="item-boton-header-finalizar" md="1" as={Col}> </ListGroup.Item>
+            </ListGroup>
+            {paginado.size > 0 ?
+                paginado.get(props.paginaActiva).map((pedido, indice) => (
+                    <ListGroup className="item-fila" key={indice.toString()} horizontal>
+                        <ListGroup.Item className="item-columna" md="2" as={Col}> {formatearFecha(pedido.fechaEntrega)} </ListGroup.Item>
+                        <ListGroup.Item className="item-columna" md="2" as={Col}> {pedido.cliente} </ListGroup.Item>
+                        <ListGroup.Item className="item-columna" md="2" as={Col}> {pedido.celular} </ListGroup.Item>
+                        <ListGroup.Item className="item-columna" md="4" as={Col}> {pedido.pedido} </ListGroup.Item>
+                        <ListGroup.Item className="item-columna" md="1" as={Col}> $ {pedido.importe} </ListGroup.Item>
+                        <ListGroup.Item className="item-boton-finalizar" md="1" as={Col}>
+                            {pedido.finalizado === false
+                                ? <Button className="boton-finalizar-pedido" variant="dark" onClick={() => props.finalizarPedido(pedido.id)}>finalizar</Button>
+                                : <Button className="boton-finalizar-pedido" variant="dark" onClick={() => props.finalizarPedido(pedido.id)} disabled>finalizado</Button>
+                            }
+                        </ListGroup.Item>
+                    </ListGroup>
+                )) : null
+            }
+        </>
     );
 };
 
