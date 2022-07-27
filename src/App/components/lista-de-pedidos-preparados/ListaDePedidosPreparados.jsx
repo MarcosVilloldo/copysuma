b236e42989565a4f87eba34a113c592fc7d45644
PC <marcosvilloldo.35122@gmail.com>
2022-07-27 01:39:13 -0300

    Se agrega fecha de entre a la lista de pedidos preparados

--- src/App/components/lista-de-pedidos-preparados/ListaDePedidosPreparados.jsx
+++ src/App/components/lista-de-pedidos-preparados/ListaDePedidosPreparados.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ListGroup, Button, Col } from 'react-bootstrap';
 import { obtenerPaginado } from '../../utils/paginado.js';
+import { formatearFecha } from '../../utils/formateador-de-fecha.js';
 import './ListaDePedidosPreparados.css';
 
 const ListaDePedidosPreparados = (props) => {
@@ -10,9 +11,10 @@ const ListaDePedidosPreparados = (props) => {
         paginado.size > 0 ?
             paginado.get(props.paginaActiva).map((pedido, indice) => (
                 <ListGroup className="item-fila" key={indice.toString()} horizontal>
+                    <ListGroup.Item className="item-columna" md="2" as={Col}> {formatearFecha(pedido.fechaEntrega)} </ListGroup.Item>
                     <ListGroup.Item className="item-columna" md="2" as={Col}> {pedido.cliente} </ListGroup.Item>
                     <ListGroup.Item className="item-columna" md="2" as={Col}> {pedido.celular} </ListGroup.Item>
-                    <ListGroup.Item className="item-columna" md="6" as={Col}> {pedido.pedido} </ListGroup.Item>
+                    <ListGroup.Item className="item-columna" md="4" as={Col}> {pedido.pedido} </ListGroup.Item>
                     <ListGroup.Item className="item-columna" md="1" as={Col}> $ {pedido.importe} </ListGroup.Item>
                     <ListGroup.Item className="item-boton-finalizar" md="1" as={Col}>
                         {pedido.finalizado === false
