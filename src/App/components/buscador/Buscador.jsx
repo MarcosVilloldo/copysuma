99079156369716122115873a25ce4f11f521f030
PC <marcosvilloldo.35122@gmail.com>
2022-07-30 19:31:23 -0300

    Se refactoriza codigo para usar module.css en el buscador

--- src/App/components/buscador/Buscador.jsx
+++ src/App/components/buscador/Buscador.jsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React from 'react';
 import { Dropdown, Form, Row, Col } from 'react-bootstrap';
-import "./Buscador.css";
+import StylesBuscador from './Buscador.module.css';
 
 const Buscador = (props) => {
     const handleInputChange = (event) => props.filtrar(event.target.value);
 
     return (
-        <Col className="box-buscador">
-            <Row className="row-datos-buscador">
-                <Col md="10">
-                    <Form.Control className="input-buscador" type="text" placeholder="Ingresar busqueda..." onChange={handleInputChange} />
+        <Col className={StylesBuscador.body}>
+            <Row className={StylesBuscador.rowBody}>
+                <Col md={10}>
+                    <Form.Control className={StylesBuscador.input} type="text" placeholder="Ingresar busqueda..." onChange={handleInputChange} />
                 </Col>
-                <Col md="2">
-                    <Dropdown className="box-boton-buscar" align='end'>
-                        <Dropdown.Toggle className="boton-buscar" variant="dark"> Buscar por {props.filtroDeBusqueda}</Dropdown.Toggle>
-                        <Dropdown.Menu className="dropdown-buscar-menu-items" variant="dark">
+                <Col md={2}>
+                    <Dropdown className={StylesBuscador.boxBotonBuscar} align='end'>
+                        <Dropdown.Toggle className={StylesBuscador.botonBuscar} variant="dark"> Buscar por {props.filtroDeBusqueda}</Dropdown.Toggle>
+                        <Dropdown.Menu className={StylesBuscador.dropdownBuscarMenuItems} variant="dark">
                             {props.filtros.map((filtro, indice) =>
                                 <Dropdown.Item onClick={() => props.modificarFiltroBusqueda(filtro)} key={indice.toString()}>{filtro}</Dropdown.Item>
                             )}
