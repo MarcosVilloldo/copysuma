7c00a1eda244030a3b96873b25866d650d64d450
PC <marcosvilloldo.35122@gmail.com>
2022-07-30 22:59:36 -0300

    Corrige la forma en la que se llama a los estilos de el buscador

--- src/App/components/buscador/Buscador.jsx
+++ src/App/components/buscador/Buscador.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Dropdown, Form, Row, Col } from 'react-bootstrap';
-import StylesBuscador from './Buscador.module.css';
+import Styles from './Buscador.module.css';
 
 const Buscador = (props) => {
     const handleInputChange = (event) => props.filtrar(event.target.value);
 
     return (
-        <Row className={StylesBuscador.body}>
+        <Row className={Styles.body}>
             <Col md={9}>
-                <Form.Control className={StylesBuscador.input} type='text' placeholder='Ingresar busqueda...' onChange={handleInputChange} />
+                <Form.Control className={Styles.input} type='text' placeholder='Ingresar busqueda...' onChange={handleInputChange} />
             </Col>
             <Col md={3}>
                 <Dropdown align='end' as={Row}>
-                    <Dropdown.Toggle className={StylesBuscador.boton} variant='dark'> Buscar por {props.filtroDeBusqueda}</Dropdown.Toggle>
-                    <Dropdown.Menu className={StylesBuscador.dropdownMenu} variant='dark'>
+                    <Dropdown.Toggle className={Styles.boton} variant='dark'> Buscar por {props.filtroDeBusqueda}</Dropdown.Toggle>
+                    <Dropdown.Menu className={Styles.dropdownMenu} variant='dark'>
                         {props.filtros.map((filtro, indice) =>
-                            <Dropdown.Item className={StylesBuscador.dropdownMenuItem} onClick={() => props.modificarFiltroBusqueda(filtro)} key={indice.toString()}>{filtro}</Dropdown.Item>
+                            <Dropdown.Item className={Styles.dropdownMenuItem} onClick={() => props.modificarFiltroBusqueda(filtro)} key={indice.toString()}>{filtro}</Dropdown.Item>
                         )}
                     </Dropdown.Menu>
                 </Dropdown>
