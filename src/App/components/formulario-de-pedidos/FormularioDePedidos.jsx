7339cf22435366e89fe30aa7721da0e9163255e6
PC <marcosvilloldo.35122@gmail.com>
2022-07-31 17:55:06 -0300

    Se refactoriza formulario de pedidos

--- src/App/components/formulario-de-pedidos/FormularioDePedidos.jsx
+++ src/App/components/formulario-de-pedidos/FormularioDePedidos.jsx
@@ -5,21 +5,21 @@ import { formatearFechaToISOString } from '../../utils/formateador-de-fecha.js';
 import ModalPedidoIngresado from '../modal-pedido-ingresado/modal-pedido-ingresado';
 import Styles from './FormularioDePedidos.module.css'
 
-const inputsDefault = {
-    id: 0,
-    cliente: null,
-    pedido: null,
-    celular: null,
-    importe: 0,
-    finalizado: false,
-    fechaPedido: null,
-    fechaEntrega: null
-}
-
 const FormularioDePedidos = (props) => {
     const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const [inputs, setInputs] = useState(inputsDefault);
+    const [inputs, setInputs] = useState(
+        {
+            id: 0,
+            cliente: null,
+            pedido: null,
+            celular: null,
+            importe: 0,
+            finalizado: false,
+            fechaPedido: null,
+            fechaEntrega: null
+        }
+    );
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
@@ -46,29 +46,29 @@ const FormularioDePedidos = (props) => {
                     <Form.Group md={4} as={Col}>
                         <Form.Label> Cliente </Form.Label>
                         <Form.Control className={Styles.input} type='text' placeholder='Ingresar cliente...' {...register('cliente', { required: 'Cliente es requerido' })} />
-                        <Col className={Styles.spam}><span className={Styles.spam}>{errors?.cliente && errors.cliente.message}</span></Col>
+                        <Col className={Styles.span}><span className={Styles.span}>{errors?.cliente && errors.cliente.message}</span></Col>
                     </Form.Group>
                     <Form.Group md={3} as={Col}>
                         <Form.Label> Celular </Form.Label>
                         <Form.Control className={Styles.input} type='text' placeholder='Ingresar celular...' {...register('celular', { required: 'Celular es requerido' })} />
-                        <Col className={Styles.spam}><span className={Styles.spam}> {errors?.celular && errors.celular.message} </span></Col>
+                        <Col className={Styles.span}><span className={Styles.span}> {errors?.celular && errors.celular.message} </span></Col>
                     </Form.Group>
                     <Form.Group md={3} as={Col}>
                         <Form.Label> Fecha de Entrega </Form.Label>
                         <Form.Control className={Styles.input} type='date' placeholder='Ingresar fecha de entrega...' {...register('fechaEntrega', { required: 'Fecha de entrega es requerida' })} />
-                        <Col className={Styles.spam}><span className={Styles.spam}> {errors?.fechaEntrega && errors.fechaEntrega.message} </span></Col>
+                        <Col className={Styles.span}><span className={Styles.span}> {errors?.fechaEntrega && errors.fechaEntrega.message} </span></Col>
                     </Form.Group>
                     <Form.Group md={2} as={Col}>
                         <Form.Label> Importe </Form.Label>
                         <Form.Control className={Styles.input} type='number' placeholder='Ingresar importe...' {...register('importe', { required: 'Importe es requerido' })} />
-                        <Col className={Styles.spam}><span className={Styles.spam}>{errors?.importe && errors.importe.message}</span></Col>
+                        <Col className={Styles.span}><span className={Styles.span}>{errors?.importe && errors.importe.message}</span></Col>
                     </Form.Group>
                 </Row>
                 <Row mb={6}>
                     <Form.Group as={Col}>
                         <Form.Label> Pedido </Form.Label>
                         <Form.Control className={Styles.input} type='text' placeholder='Ingresar pedido...' {...register('pedido', { required: 'Pedido es requerido' })} />
-                        <Col className={Styles.spam}><span className={Styles.spam}>{errors?.pedido && errors.pedido.message}</span></Col>
+                        <Col className={Styles.span}><span className={Styles.span}>{errors?.pedido && errors.pedido.message}</span></Col>
                     </Form.Group>
                 </Row>
                 <Row className={Styles.rowBoton}>
