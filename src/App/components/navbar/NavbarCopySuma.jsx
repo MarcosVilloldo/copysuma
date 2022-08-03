b9ac74965dad9e3f146915f589351430a93e3173
PC <marcosvilloldo.35122@gmail.com>
2022-08-03 19:41:02 -0300

    Se modifica estilos para utilizar module.css en el NAVBAR

--- src/App/components/navbar/NavbarCopySuma.jsx
+++ src/App/components/navbar/NavbarCopySuma.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Logo from "../../img/logo-copysuma.png";
 import { Navbar, Col } from 'react-bootstrap';
-import "./NavbarCopySuma.css";
+import Style from "./NavbarCopySuma.module.css";
 
 const NavbarCopySuma = () => (
-    <Navbar className="navbar" bg="light" expand="lg">
+    <Navbar className={Style.navbar} bg="light" expand="lg">
         <Navbar.Brand as={Col}><img src={Logo} /></Navbar.Brand>
     </Navbar>
 );
