9be13617f593aab6123f759ef1bce986096084bc
PC <marcosvilloldo.35122@gmail.com>
2022-08-03 22:05:26 -0300

    Refactor de codifo de lista de pedidos y lista de pedidos preparados

--- src/App/components/lista-de-pedidos/ListaDePedidos.jsx
+++ src/App/components/lista-de-pedidos/ListaDePedidos.jsx
@@ -22,32 +22,32 @@ const ListaDePedidos = (props) => {
     return (
         <>
             <ListGroup className={props.StylesTabla.itemFila} horizontal>
-                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md="1" as={Col}> Ingresado </ListGroup.Item>
-                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md="1" as={Col}> Entrega </ListGroup.Item>
-                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md="2" as={Col}> Cliente </ListGroup.Item>
-                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md="1" as={Col}> Celular </ListGroup.Item>
-                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md="4" as={Col}> Pedido </ListGroup.Item>
-                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} as={Col}> Importe </ListGroup.Item>
-                <ListGroup.Item className={Styles.itemColumnaHeaderBotones} md="2" as={Col}> </ListGroup.Item>
+                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={1} as={Col}> Ingresado </ListGroup.Item>
+                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={1} as={Col}> Entrega </ListGroup.Item>
+                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={2} as={Col}> Cliente </ListGroup.Item>
+                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={1} as={Col}> Celular </ListGroup.Item>
+                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={4} as={Col}> Pedido </ListGroup.Item>
+                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={1} as={Col}> Importe </ListGroup.Item>
+                <ListGroup.Item className={Styles.itemColumnaHeaderBotones} md={2} as={Col}> </ListGroup.Item>
             </ListGroup>
             {paginado.size > 0 ?
                 paginado.get(props.paginaActiva).map((pedido, cantidad) => (
                     <ListGroup className={props.StylesTabla.itemFila} key={cantidad.toString()} horizontal>
-                        <ListGroup.Item className={props.StylesTabla.itemColumna} md="1" as={Col}> {formatearFecha(pedido.fechaPedido)} </ListGroup.Item>
-                        <ListGroup.Item className={props.StylesTabla.itemColumna} md="1" as={Col}> {formatearFecha(pedido.fechaEntrega)} </ListGroup.Item>
-                        <ListGroup.Item className={props.StylesTabla.itemColumna} md="2" as={Col}> {pedido.cliente} </ListGroup.Item>
-                        <ListGroup.Item className={props.StylesTabla.itemColumna} md="1" as={Col}> {pedido.celular} </ListGroup.Item>
-                        <ListGroup.Item className={props.StylesTabla.itemColumna} md="4" as={Col}> {pedido.pedido} </ListGroup.Item>
-                        <ListGroup.Item className={props.StylesTabla.itemColumna} md="1" as={Col}> $ {pedido.importe} </ListGroup.Item>
-                        <ListGroup.Item className={Styles.itemColumnaBotones} md="2" as={Col}>
-                            <Row className={Styles.botonera} md="12">
-                                <Col className={Styles.boxBotonEditar} md="4">
-                                    <Button className={Styles.botonEditar} variant="dark" onClick={() => mostrarModalEditarPedido(pedido)}>Editar</Button>
+                        <ListGroup.Item className={props.StylesTabla.itemColumna} md={1} as={Col}> {formatearFecha(pedido.fechaPedido)} </ListGroup.Item>
+                        <ListGroup.Item className={props.StylesTabla.itemColumna} md={1} as={Col}> {formatearFecha(pedido.fechaEntrega)} </ListGroup.Item>
+                        <ListGroup.Item className={props.StylesTabla.itemColumna} md={2} as={Col}> {pedido.cliente} </ListGroup.Item>
+                        <ListGroup.Item className={props.StylesTabla.itemColumna} md={1} as={Col}> {pedido.celular} </ListGroup.Item>
+                        <ListGroup.Item className={props.StylesTabla.itemColumna} md={4} as={Col}> {pedido.pedido} </ListGroup.Item>
+                        <ListGroup.Item className={props.StylesTabla.itemColumna} md={1} as={Col}> $ {pedido.importe} </ListGroup.Item>
+                        <ListGroup.Item className={Styles.itemColumnaBotones} md={2} as={Col}>
+                            <Row className={Styles.botonera} md={12}>
+                                <Col className={Styles.boxBotonEditar} md={4}>
+                                    <Button className={Styles.botonEditar} variant={'dark'} onClick={() => mostrarModalEditarPedido(pedido)}>Editar</Button>
                                 </Col>
-                                <Col className={Styles.boxBotonPreparar} md="8">
+                                <Col className={Styles.boxBotonPreparar} md={8}>
                                     {pedido.finalizado === false
-                                        ? <Button className={Styles.botonPreparar} variant="dark" onClick={() => props.prepararPedido(pedido.id)}>Preparar</Button>
-                                        : <Button className={Styles.botonPreparar} variant="dark" onClick={() => props.prepararPedido(pedido.id)} disabled>Preparado</Button>
+                                        ? <Button className={Styles.botonPreparar} variant={'dark'} onClick={() => props.prepararPedido(pedido.id)}>Preparar</Button>
+                                        : <Button className={Styles.botonPreparar} variant={'dark'} onClick={() => props.prepararPedido(pedido.id)} disabled>Preparado</Button>
                                     }
                                 </Col>
                             </Row>
