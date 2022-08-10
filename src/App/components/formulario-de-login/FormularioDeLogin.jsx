c70dbe45f533402625ecc512474c2f38c564db76
PC <marcosvilloldo.35122@gmail.com>
2022-08-09 21:51:00 -0300

    Se le agregan estilos al login

--- src/App/components/formulario-de-login/FormularioDeLogin.jsx
+++ src/App/components/formulario-de-login/FormularioDeLogin.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { useForm } from 'react-hook-form';
-import { Button, Form, Row, Col } from 'react-bootstrap';
+import { Image, Button, Form, Row, Col } from 'react-bootstrap';
 import Logo from "../../img/logo-copysuma.png";
 import Styles from './FormularioDeLogin.module.css';
 
 const FormularioDeLogin = () => {
     return (
-        <Form className={Styles.body}>
-            <img src={Logo} />
-            <Form.Group >
+        <Form className={Styles.body} as={Row}>
+            <Image className={Styles.image} src={Logo} as={Col} />
+            <Form.Group className={Styles.input}>
                 <Form.Label> Usuario </Form.Label>
                 <Form.Control type='text' />
             </Form.Group>
-            <Form.Group >
+            <Form.Group className={Styles.input}>
                 <Form.Label> Contraseña </Form.Label>
                 <Form.Control type='text' />
             </Form.Group>
-            <Button variant='dark' type='submit'> Iniciar </Button>
+            <Button className={Styles.boton} variant='dark' type='submit'> Iniciar </Button>
         </Form>
     );
 };
