10bd76eebcfac9c068fba8b180bc9d2468018446
PC <marcosvilloldo.35122@gmail.com>
2022-08-10 21:02:54 -0300

    Se agrega validacion para que se muestre el login o la vista principal del proyecto

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,26 +7,32 @@ import NavbarCopySuma from "./components/navbar/NavbarCopySuma";
 import Navegador from "./components/navegador/Navegador";
 import Preparados from "./views/preparados/preparados";
 import Estadisticas from "./views/estadisticas/estadisticas";
+import Login from './views/login/login';
 import "./App.css";
 
 const PATH = "/copysuma";
+const seLogueo = false;
 
-const App = () => (
-    <>
-        <NavbarCopySuma />
-        <Col className="cuerpo-principal">
-            <BrowserRouter>
-                <Routes>
-                    <Route path={PATH} element={<Navegador path={PATH} />}>
-                        <Route index element={<Home />} />
-                        <Route path={PATH + "/pedidos-preparados"} element={<Preparados />} />
-                        <Route path={PATH + "/biblioteca"} element={<Biblioteca />} />
-                        <Route path={PATH + "/estadisticas"} element={<Estadisticas />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </Col>
-    </>
-);
+const App = () => {
+    return (
+        seLogueo ?
+            <>
+                <NavbarCopySuma />
+                <Col className="cuerpo-principal">
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path={PATH} element={<Navegador path={PATH} />}>
+                                <Route index element={<Home />} />
+                                <Route path={PATH + "/pedidos-preparados"} element={<Preparados />} />
+                                <Route path={PATH + "/biblioteca"} element={<Biblioteca />} />
+                                <Route path={PATH + "/estadisticas"} element={<Estadisticas />} />
+                            </Route>
+                        </Routes>
+                    </BrowserRouter>
+                </Col>
+            </>
+            : <Login />
+    );
+};
 
 export default App;
