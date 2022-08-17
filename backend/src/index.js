fed470d6ca8e83def8313169f2ebb318dd933f8a
PC <marcosvilloldo.35122@gmail.com>
2022-08-17 02:01:09 -0300

    Agrega dependencias de morgan y body parser al backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.disable('x-powered-by');
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.end('CopySUMA')
