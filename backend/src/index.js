749365bed8062bc4d5b63e8c05c4c290e4549dc1
PC <marcosvilloldo.35122@gmail.com>
2022-08-17 15:00:57 -0300

    Se agrega la dependencia hbs para el manejo de plantillas y se define ruta y vista para index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
 
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.disable('x-powered-by');
 
@@ -10,9 +16,7 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-    res.end('CopySUMA')
-})
+app.use('/', indexRouter); 
 
 app.listen('9000', () => {
     console.log('Servidor arranco')
