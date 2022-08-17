e53ac97886fd571b1e0fb5201b606761d557908d
PC <marcosvilloldo.35122@gmail.com>
2022-08-17 02:22:04 -0300

    Se agrega dependecia de pug como primera instancia para el manejo de plantillas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -10,8 +11,11 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-    res.end('CopySUMA')
+    res.render('home')
 })
 
 app.listen('9000', () => {
