b31152237aad0c719e42967261c976e7a17c07cf
PC <marcosvilloldo.35122@gmail.com>
2022-08-17 01:47:53 -0300

    Se crea nodemon.json para la configuracion de nodemon

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,11 @@
-console.log('CopySUMA')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.end('CopySUMA')
+})
+
+app.listen('9000', () => {
+    console.log('Servidor arranco')
+})
