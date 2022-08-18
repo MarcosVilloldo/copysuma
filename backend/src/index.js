f4a6aef29dc4e738a48d5841c884c9916bf6d6ef
PC <marcosvilloldo.35122@gmail.com>
2022-08-18 20:38:23 -0300

    Se agrega dependecia de dotenv para la credenciales de la base de datos

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,8 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-
-const host = 'mongodb://127.0.0.1:27017/copysuma'
 const indexRouter = require('./routes/index');
+const dotenv = require('dotenv').config();
 
 const app = express();
 
@@ -23,7 +22,7 @@ app.use('/', indexRouter);
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
 
-const conn = mongoose.createConnection(host)
+const conn = mongoose.createConnection(process.env.MONGO_HOST, { dbName: process.env.MONGO_DB_NAME })
 
 conn.on('error', err => {
     console.log('Error', err);
