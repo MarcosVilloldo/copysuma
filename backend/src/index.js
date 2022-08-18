7e27dfc6014fefe84305e9b766197240905154fa
PC <marcosvilloldo.35122@gmail.com>
2022-08-18 16:17:57 -0300

    Se agrega conexion a  mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const host = 'mongodb://127.0.0.1:27017/copysuma'
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -14,8 +16,20 @@ app.disable('x-powered-by');
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', indexRouter); 
+app.use('/', indexRouter);
+
+mongoose.set('debug', true);
+mongoose.Promise = global.Promise;
+
+const conn = mongoose.createConnection(host)
+
+conn.on('error', err => {
+    console.log('Error', err);
+    return process.exit();
+})
+
+conn.on('connected', () => console.log('Conectado a mongo'))
 
 module.exports = app;
