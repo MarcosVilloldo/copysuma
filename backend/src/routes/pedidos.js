7cad47835bfa7d0a1663693194a04ed012131be6
PC <marcosvilloldo.35122@gmail.com>
2022-08-20 21:03:42 -0300

    Se hace consulta para agregar un nuevo pedido a la api

--- backend/src/routes/pedidos.js
+++ backend/src/routes/pedidos.js
@@ -2,10 +2,27 @@ const express = require('express');
 const router = express.Router();
 const pedidosModel = require('../models/pedidos-model');
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
     try {
         let data = await pedidosModel.find({});
         res.send(data);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+router.post('/agregar', async (req, res) => {
+    try {
+        await pedidosModel.insertMany({
+            cliente: req.body.cliente,
+            pedido: req.body.pedido,
+            celular: req.body.celular,
+            importe: req.body.importe,
+            finalizado: req.body.finalizado,
+            fechaPedido: req.body.fechaPedido,
+            fechaEntrega: req.body.fechaEntrega
+        })
+        res.send(res);
     } catch (error) {
         res.send(error);
     }
