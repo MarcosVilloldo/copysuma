141bd39c1cd6c1f094fccf75221b5bf11d179a7d
PC <marcosvilloldo.35122@gmail.com>
2022-08-26 22:19:50 -0300

    Corrige nombre de dashboard

--- frontend/src/app/views/estadisticas/estadisticas.jsx
+++ frontend/src/app/views/estadisticas/estadisticas.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Dashboard from "../../components/dashboard/dashboard";
+import Dashboard from "../../components/dashboard/Dashboard";
 
 const Estadisticas = () => (
     <>
