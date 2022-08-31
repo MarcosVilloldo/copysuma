9a449742d81877ef2cb0f049eedb7dd3fe39b58e
PC <marcosvilloldo.35122@gmail.com>
2022-08-31 10:49:24 -0300

    Se saca el readme de las carpetas de frontend y backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const session = require('express-session');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const router = {
