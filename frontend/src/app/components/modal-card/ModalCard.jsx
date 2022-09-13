ef38920b8f67de293b11afad7ca71c762379ae39
PC <marcosvilloldo.35122@gmail.com>
2022-09-13 09:51:50 -0300

    Correcciones al modalCard

--- frontend/src/app/components/modal-card/ModalCard.jsx
+++ frontend/src/app/components/modal-card/ModalCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Button, Row, Image, Card } from 'react-bootstrap';
+import { Modal, Button, Row, Image } from 'react-bootstrap';
 import Styles from "./ModalCard.module.css";
 
 const RUTA_PORTADAS = require.context('../../img', true);
@@ -11,7 +11,7 @@ const ModalCard = (props) => {
             <Modal.Body className={Styles.body}>
                 {props.moduloActivo.portada ? <Row><Image className={Styles.image} src={RUTA_PORTADAS(`./${props.moduloActivo.portada}`)} rounded /></Row> : <></>}
                 <Row className={Styles.titulo}>{props.moduloActivo.titulo}</Row>
-                {props.moduloActivo.descripcion ? <Row>{props.moduloActivo.descripcion}</Row> : <Row> Sin descripcion </Row>}
+                <Row>{props.moduloActivo.descripcion}</Row>
             </Modal.Body>
             <Modal.Footer className={Styles.footer}>
                 <Button variant="secondary" onClick={props.handleClose}>Cancelar</Button>
