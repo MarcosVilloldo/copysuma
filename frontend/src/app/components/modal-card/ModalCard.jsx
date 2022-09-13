7104b109356cc496da03c016d2e5cf9a3d492b75
PC <marcosvilloldo.35122@gmail.com>
2022-09-13 09:04:20 -0300

    Se agrega estilos al modal de la card de vista previa

--- frontend/src/app/components/modal-card/ModalCard.jsx
+++ frontend/src/app/components/modal-card/ModalCard.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { Modal, Button, Row, Image } from 'react-bootstrap';
+import { Modal, Button, Row, Image, Card } from 'react-bootstrap';
 import Styles from "./ModalCard.module.css";
 
 const RUTA_PORTADAS = require.context('../../img', true);
 
 const ModalCard = (props) => {
     return (
-        <Modal show={props.show} size={'lg'} backdrop='static' keyboard={false} centered>
-            <Modal.Body>
-                {props.moduloActivo.portada ? <Image src={RUTA_PORTADAS(`./${props.moduloActivo.portada}`)}></Image> : <></>}
-                <Row className={Styles.descripcion}>Descripción</Row>
-                <Row>{props.moduloActivo.descripcion}</Row>
+        <Modal show={props.show} backdrop='static' keyboard={false} centered>
+            <Modal.Header className={Styles.header} />
+            <Modal.Body className={Styles.body}>
+                {props.moduloActivo.portada ? <Row><Image className={Styles.image} src={RUTA_PORTADAS(`./${props.moduloActivo.portada}`)} rounded /></Row> : <></>}
+                <Row className={Styles.titulo}>{props.moduloActivo.titulo}</Row>
+                {props.moduloActivo.descripcion ? <Row>{props.moduloActivo.descripcion}</Row> : <Row> Sin descripcion </Row>}
             </Modal.Body>
-            <Modal.Footer>
+            <Modal.Footer className={Styles.footer}>
                 <Button variant="secondary" onClick={props.handleClose}>Cancelar</Button>
             </Modal.Footer>
         </Modal>
