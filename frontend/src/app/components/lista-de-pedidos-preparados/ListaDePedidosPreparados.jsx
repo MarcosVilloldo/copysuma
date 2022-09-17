05334a148dfc635e5b5b11964d6a45694fa7be54
PC <marcosvilloldo.35122@gmail.com>
2022-09-17 17:58:42 -0300

    Se pone fecha de finalizado cuando se finaliza un pedido preparado

--- frontend/src/app/components/lista-de-pedidos-preparados/ListaDePedidosPreparados.jsx
+++ frontend/src/app/components/lista-de-pedidos-preparados/ListaDePedidosPreparados.jsx
@@ -22,25 +22,25 @@ const ListaDePedidosPreparados = (props) => {
     return (
         <>
             <ListGroup className={props.StylesTabla.itemFila} horizontal>
-                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={2} as={Col}> Entrega </ListGroup.Item>
+                <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={1} as={Col}> Entrega </ListGroup.Item>
                 <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={2} as={Col}> Cliente </ListGroup.Item>
                 <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={2} as={Col}> Celular </ListGroup.Item>
                 <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={4} as={Col}> Pedido </ListGroup.Item>
                 <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={1} as={Col}> Importe </ListGroup.Item>
-                <ListGroup.Item className={Style.itemBotonHeaderFinalizar} md={1} as={Col}> </ListGroup.Item>
+                <ListGroup.Item className={Style.itemBotonHeaderFinalizar} md={2} as={Col}> </ListGroup.Item>
             </ListGroup>
             {paginado.size > 0 ?
                 paginado.get(props.paginaActiva).map((pedido, indice) => (
                     <ListGroup className={props.StylesTabla.itemFila} key={indice.toString()} horizontal>
-                        <ListGroup.Item className={props.StylesTabla.itemColumna} md={2} as={Col}> {formatearFecha(pedido.fechaEntrega)} </ListGroup.Item>
+                        <ListGroup.Item className={props.StylesTabla.itemColumna} md={1} as={Col}> {formatearFecha(pedido.fechaEntrega)} </ListGroup.Item>
                         <ListGroup.Item className={props.StylesTabla.itemColumna} md={2} as={Col}> {pedido.cliente} </ListGroup.Item>
                         <ListGroup.Item className={props.StylesTabla.itemColumna} md={2} as={Col}> {pedido.celular} </ListGroup.Item>
                         <ListGroup.Item className={props.StylesTabla.itemColumna} md={4} as={Col}> {pedido.pedido} </ListGroup.Item>
                         <ListGroup.Item className={props.StylesTabla.itemColumna} md={1} as={Col}> $ {pedido.importe} </ListGroup.Item>
-                        <ListGroup.Item className={Style.itemBotonFinalizar} md={1} as={Col}>
+                        <ListGroup.Item className={Style.itemBotonFinalizar} md={2} as={Col}>
                             {pedido.finalizado === false
                                 ? <Button className={Style.botonFinalizar} variant={'dark'} onClick={() => mostrarModalConfirmar(pedido)}>finalizar</Button>
-                                : <Button className={Style.botonFinalizar} variant={'dark'} disabled>finalizado</Button>
+                                : <Col> finalizado {formatearFecha(pedido.fechaBaja)}</Col>
                             }
                         </ListGroup.Item>
                     </ListGroup>
