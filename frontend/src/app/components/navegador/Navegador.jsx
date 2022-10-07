5c47f4d1530db1f1c99d9054be110f9a89581ba9
PC <marcosvilloldo.35122@gmail.com>
2022-10-07 01:15:52 -0300

    Se corrige parametros de navliks

--- frontend/src/app/components/navegador/Navegador.jsx
+++ frontend/src/app/components/navegador/Navegador.jsx
@@ -3,17 +3,33 @@ import { Outlet, NavLink } from "react-router-dom";
 import { Nav } from 'react-bootstrap';
 import Styles from "./Navegador.module.css"
 
-const Navegador = (props) => (
-    <>
-        <Nav className={Styles.nav} variant={'pills'} justify>
-            <NavLink exact className={({ isActive }) => "nav-item " + Styles.navLink + (isActive ? " active" : "")} to={props.path + "/pedidos"}>Pedidos</NavLink>
-            <NavLink exact className={({ isActive }) => "nav-item " + Styles.navLink + (isActive ? " active" : "")} to={props.path + "/preparados"}>Pedidos preparados</NavLink>
-            <NavLink exact className={({ isActive }) => "nav-item " + Styles.navLink + (isActive ? " active" : "")} to={props.path + "/biblioteca"}>Biblioteca</NavLink>
-            {props.session.nivelPermiso === 2 ? <NavLink exact className={({ isActive }) => "nav-item " + Styles.navLink + (isActive ? " active" : "")} to={props.path + "/estadisticas"}>Estadísticas</NavLink> : <></>}
-        </Nav>
-        <hr />
-        <Outlet />
-    </>
-);
+const Navegador = (props) => {
+    let estiloActivo = { color: 'white', background: '0 0', backgroundColor: 'rgb(44, 44, 44)', borderRadius: '0.25rem' };
+
+    return (
+        <>
+            <Nav className={Styles.nav} variant={'pills'} justify>
+                <NavLink className={({ isActive }) => "nav-item " + Styles.navLink + (isActive ? " active" : "")}
+                    style={({ isActive }) => (isActive ? estiloActivo : undefined)}
+                    to={props.path + "/pedidos"}>Pedidos
+                </NavLink>
+                <NavLink className={({ isActive }) => "nav-item " + Styles.navLink + (isActive ? " active" : "")}
+                    style={({ isActive }) => (isActive ? estiloActivo : undefined)}
+                    to={props.path + "/preparados"}>Pedidos preparados
+                </NavLink>
+                <NavLink className={({ isActive }) => "nav-item " + Styles.navLink + (isActive ? " active" : "")}
+                    style={({ isActive }) => (isActive ? estiloActivo : undefined)}
+                    to={props.path + "/biblioteca"}>Biblioteca</NavLink>
+                {props.session.nivelPermiso === 2 ?
+                    <NavLink className={({ isActive }) => "nav-item " + Styles.navLink + (isActive ? " active" : "")}
+                        style={({ isActive }) => (isActive ? estiloActivo : undefined)}
+                        to={props.path + "/estadisticas"}>Estadísticas</NavLink> : <></>
+                }
+            </Nav>
+            <hr />
+            <Outlet />
+        </>
+    );
+};
 
 export default Navegador;
