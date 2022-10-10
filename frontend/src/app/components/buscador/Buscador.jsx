7ea04e609143f72b327d8a0a181740761e801c6b
PC <marcosvilloldo.35122@gmail.com>
2022-10-10 18:47:27 -0300

    Se hace buscador responsive

--- frontend/src/app/components/buscador/Buscador.jsx
+++ frontend/src/app/components/buscador/Buscador.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Dropdown, Form, Row, Col } from 'react-bootstrap';
+import { Dropdown, Form, Container, Row, Col } from 'react-bootstrap';
 import Styles from './Buscador.module.css';
 
 const Buscador = (props) => {
     const handleInputChange = (event) => props.filtrar(event.target.value);
 
     return (
-        <Row className={Styles.body}>
-            <Col md={9}>
-                <Form.Control className={Styles.input} type='text' placeholder='Ingresar busqueda...' onChange={handleInputChange} />
+        <Container className={Styles.contenedor}>
+            <Col className={Styles.input}>
+                <Form.Control className={Styles.texto} type='text' placeholder='Ingresar busqueda...' onChange={handleInputChange} />
             </Col>
-            <Col md={3}>
+            <Col className={Styles.boton}>
                 <Dropdown align='end' as={Row}>
-                    <Dropdown.Toggle className={Styles.boton} variant='dark'> Buscar por {props.filtroDeBusqueda}</Dropdown.Toggle>
+                    <Dropdown.Toggle className={Styles.dropdownToggle} variant='dark'> Buscar por {props.filtroDeBusqueda}</Dropdown.Toggle>
                     <Dropdown.Menu className={Styles.dropdownMenu} variant='dark'>
                         {props.filtros.map((filtro, indice) =>
                             <Dropdown.Item className={Styles.dropdownMenuItem} onClick={() => props.modificarFiltroBusqueda(filtro)} key={indice.toString()}>{filtro}</Dropdown.Item>
@@ -20,7 +20,7 @@ const Buscador = (props) => {
                     </Dropdown.Menu>
                 </Dropdown>
             </Col>
-        </Row>
+        </Container>
     )
 };
 
