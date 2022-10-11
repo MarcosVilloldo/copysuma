fbcdb88f0849c7307acfa0145023be8d08dd1a9a
PC <marcosvilloldo.35122@gmail.com>
2022-10-10 23:05:05 -0300

    Se hace responsive la lista de pedidos preparados

--- frontend/src/app/components/lista-de-pedidos-preparados/ListaDePedidosPreparados.jsx
+++ frontend/src/app/components/lista-de-pedidos-preparados/ListaDePedidosPreparados.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { ListGroup, Button, Col } from 'react-bootstrap';
+import { ListGroup, Button, Container, Col } from 'react-bootstrap';
 import { obtenerPaginado } from '../../utils/paginado.js';
 import { formatearFecha } from '../../utils/formateador-de-fecha.js';
 import ModalConfirmar from "../modal-confirmar/ModalConfirmar.jsx";
-import Style from './ListaDePedidosPreparados.module.css';
+import Styles from './ListaDePedidosPreparados.module.css';
 
 const ListaDePedidosPreparados = (props) => {
     const paginado = obtenerPaginado(props.pedidos, props.paginas);
@@ -20,14 +20,14 @@ const ListaDePedidosPreparados = (props) => {
     }
 
     return (
-        <>
+        <Container className={Styles.contenedor}>
             <ListGroup className={props.StylesTabla.itemFila} horizontal>
                 <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={1} as={Col}> Entrega </ListGroup.Item>
                 <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={2} as={Col}> Cliente </ListGroup.Item>
                 <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={2} as={Col}> Celular </ListGroup.Item>
                 <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={4} as={Col}> Pedido </ListGroup.Item>
                 <ListGroup.Item className={props.StylesTabla.itemColumnaHeader} md={1} as={Col}> Importe </ListGroup.Item>
-                <ListGroup.Item className={Style.itemBotonHeaderFinalizar} md={2} as={Col}> </ListGroup.Item>
+                <ListGroup.Item className={Styles.itemBotonHeaderFinalizar} md={2} as={Col}> </ListGroup.Item>
             </ListGroup>
             {paginado.size > 0 ?
                 paginado.get(props.paginaActiva).map((pedido, indice) => (
@@ -37,17 +37,17 @@ const ListaDePedidosPreparados = (props) => {
                         <ListGroup.Item className={props.StylesTabla.itemColumna} md={2} as={Col}> {pedido.celular} </ListGroup.Item>
                         <ListGroup.Item className={props.StylesTabla.itemColumna} md={4} as={Col}> {pedido.pedido} </ListGroup.Item>
                         <ListGroup.Item className={props.StylesTabla.itemColumna} md={1} as={Col}> $ {pedido.importe} </ListGroup.Item>
-                        <ListGroup.Item className={Style.itemBotonFinalizar} md={2} as={Col}>
+                        <ListGroup.Item className={Styles.itemBotonFinalizar} md={2} as={Col}>
                             {pedido.finalizado === false
-                                ? <Button className={Style.botonFinalizar} variant={'dark'} onClick={() => mostrarModalConfirmar(pedido)}>Finalizar</Button>
-                                : <Col className={Style.finalizado}> Finalizado el {formatearFecha(pedido.fechaBaja)}</Col>
+                                ? <Button className={Styles.botonFinalizar} variant={'dark'} onClick={() => mostrarModalConfirmar(pedido)}>Finalizar</Button>
+                                : <Col className={Styles.finalizado}> Finalizado el {formatearFecha(pedido.fechaBaja)}</Col>
                             }
                         </ListGroup.Item>
                     </ListGroup>
                 )) : null
             }
             <ModalConfirmar pedidoActivo={pedidoActivo} accion={'finalizar'} confirmar={props.finalizarPedido} show={showModalConfirmar} handleClose={handleCloseModalConfirmar} />
-        </>
+        </Container>
     );
 };
 
